e7af5f31a09ca7bc2d83e6ad25ab006bc6c0bfcc
Shufan Shahi <shufanshahi@gmail.com>
2025-08-06 23:51:43 +0600

    video call working

---
+++ server/src/main/java/com/example/postfolio/config/SocketIOConfig.java
@@ -0,0 +1,25 @@
+package com.example.postfolio.config;
+
+import com.corundumstudio.socketio.SocketIOServer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SocketIOConfig {
+
+    @Value("${socket.host}")
+    private String host;
+
+    @Value("${socket.port}")
+    private int port;
+
+    @Bean
+    public SocketIOServer socketIOServer() {
+        com.corundumstudio.socketio.Configuration config = new com.corundumstudio.socketio.Configuration();
+        config.setHostname(host);
+        config.setPort(port);
+        config.setOrigin("*");
+        return new SocketIOServer(config);
+    }
+}
